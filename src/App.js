85d4232b2bec40779782c56ea07e3431b0d6e814
“Elvira55872” <elli-x1996@mail.ru>
2024-06-28 13:28:19 +0200

    classwork 02

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
 
+// Lessons
+// import Lesson02 from "./lessons/lesson02/lesson02";
+
+//Homeworks
+import Homework02 from "./homeworks/homework02/Homework02";
+
 function App() {
   return (
     <div className="App">
-      <p>This is Elvira`s first react app</p>
+      {/* <p>This is Elvira`s first react app</p> */}
+      {/* <Lesson02 /> */}
+      <Homework02 />
     </div>
   );
 }
---
+++ src/lessons/lesson02/lesson02.js
@@ -0,0 +1,14 @@
+import Button from "../../components/Button/Button";
+import "./styles.css";
+
+function Lesson02() {
+  return (
+    <div className="lesson02-component">
+      <Button />
+      <div>Lesson02</div>
+      <Button />
+    </div>
+  );
+}
+
+export default Lesson02;
