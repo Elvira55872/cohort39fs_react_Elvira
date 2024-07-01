76f8fa4e32579f090645d25528ce4116aa81e3b8
“Elvira55872” <elli-x1996@mail.ru>
2024-07-01 19:30:36 +0200

    deleted app.css wrong styles, corrected my mistake

--- src/homeworks/homework03/Homework03.jsx
+++ src/homeworks/homework03/Homework03.jsx
@@ -2,7 +2,11 @@ import LoginForm from "../../components/LoginForm/LoginForm";
 import "./styles.css";
 
 function Homework03() {
-  return <LoginForm />;
+  return (
+    <div className="homework03-wrapper">
+      <LoginForm />
+    </div>
+  );
 }
 
 export default Homework03;
